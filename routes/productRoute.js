072157fd854c66e665d8783be4b0c32cb10e87e8
Ivan Vostrikov <ivanvostrikov@MacBook-Air-Ivan.local>
2020-04-02 19:15:56 +0700

    fix get req for product route

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -4,39 +4,40 @@ const Product = require('../models/productSchema');
 const router = Router();
 
 router.get('/', async (req, res) => {
-    try {
-        res.json(await Product.find());
-    } catch (e) {
-        console.log(e);
-    }
+  try {
+    const products = await  Product.find();
+    res.json({products});
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 router.get('/:id', async (req, res) => {
-    try {
-        res.json(await Product.findById(req.params.id));
-    } catch (e) {
-        console.log(e);
-    }
+  try {
+    res.json(await Product.findById(req.params.id));
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 router.post('/', async (req, res) => {
-    try {
-        const product = new Product({
-            model: req.body.model,
-            article: req.body.article,
-            price: req.body.price,
-            gender: req.body.gender,
-            images: req.body.images,
-            color: req.body.color,
-            discountPrice: req.body.discountPrice,
-            description: req.body.description
-        });
+  try {
+    const product = new Product({
+      model: req.body.model,
+      article: req.body.article,
+      price: req.body.price,
+      gender: req.body.gender,
+      images: req.body.images,
+      color: req.body.color,
+      discountPrice: req.body.discountPrice,
+      description: req.body.description
+    });
 
-        await product.save();
-        res.json({ state: 'success' });
-    } catch (e) {
-        console.log(e);
-    }
+    await product.save();
+    res.json({state: 'success'});
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 module.exports = router;
